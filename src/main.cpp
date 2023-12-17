90937051af724fa527ada8582d5fe972499cff53
Qnurye <Qnurye@gmail.com>
2023-12-17 23:06:19 +0800

    BUbsuiabsfb

--- src/main.cpp
+++ src/main.cpp
@@ -19,33 +19,57 @@ void setup() {
     servoL.initialize();
     servoR.initialize();
     Serial.begin(9600);
+
+    while (!sensorL.readSensor()) {
+        // 矫正左边
+        if (sensorL.readSensor()) {
+            Serial.print("Left is OK !");
+        }
+        stepDriverL.turn(Clockwise);
+    }
+
+    while (!sensorR.readSensor()) {
+        // 矫正右边
+        if (sensorR.readSensor()) {
+            Serial.print("Right is OK !");
+        }
+        stepDriverR.turn(Clockwise);
+    }
 }
 
 void loop() {
+
+    if (sensorL.readSensor()) {
+        Serial.println("Left is OK !");
+    }
+    if (sensorR.readSensor()) {
+        Serial.println("Right is OK !");
+    }
+    delay(50);
 //    servoL.setAngle(90);
 //    delay(1000);
 //    servoR.setAngle(90);
 //    delay(1000);
 
-//    bool sensorValue = sensorL.readSensor();
-//    Serial.println(sensorValue ? "1\n" : "0\n");
-//    delay(10);
+//    if(sensorL.readSensor() || sensorR.readSensor()) {
+//        Serial.println("Oh Light !");
+//    }
 
-    delay(1000);  // 延时1秒
-
-    stepDriverL.turn(Clockwise);
-    stepDriverR.turn(Clockwise);
-
-    delay(500);
-    stepDriverL.turn(Counterclockwise);
-    stepDriverR.turn(Counterclockwise);
-
-    delay(1000);
-
-    stepDriverL.turn(Clockwise);
-    stepDriverR.turn(Clockwise);
-    delay(500);
-
-    stepDriverL.turn(Stop);
-    stepDriverR.turn(Stop);
+//    delay(1000);  // 延时1秒
+//
+//    stepDriverL.turn(Clockwise);
+//    stepDriverR.turn(Clockwise);
+//
+//    delay(500);
+//    stepDriverL.turn(Counterclockwise);
+//    stepDriverR.turn(Counterclockwise);
+//
+//    delay(1000);
+//
+//    stepDriverL.turn(Clockwise);
+//    stepDriverR.turn(Clockwise);
+//    delay(500);
+//
+//    stepDriverL.turn(Stop);
+//    stepDriverR.turn(Stop);
 }
