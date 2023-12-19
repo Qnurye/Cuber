18376a995d78f7bf9f328be2745c87a1979214ff
Qnurye <Qnurye@gmail.com>
2023-12-19 17:09:04 +0800

    修复右舵机问题

--- src/main.cpp
+++ src/main.cpp
@@ -6,40 +6,41 @@
 #include "EESX670.h"
 #include "Controllers.h"
 
-EE_SX670 sensorL(L_LOC_SENSOR_PIN);
-EE_SX670 sensorR(R_LOC_SENSOR_PIN);
+EE_SX670 sensorL(R_STEP_PUL_PIN);
+EE_SX670 sensorR(R_LOC_SENSOR_PIN); // #10 脚位用不了 pwm, 换了个口
 TB660 stepDriverL(L_STEP_DIR_PIN, L_STEP_PUL_PIN, DEFAULT_DUTY_CYCLE);
-TB660 stepDriverR(R_STEP_DIR_PIN, R_STEP_PUL_PIN, DEFAULT_DUTY_CYCLE);
+TB660 stepDriverR(R_STEP_DIR_PIN, L_LOC_SENSOR_PIN, DEFAULT_DUTY_CYCLE);
 MEGA996R servoL(L_SERVO_PIN);
 MEGA996R servoR(R_SERVO_PIN);
 
 char cmd;
+
 void setup() {
-    sensorL.initialize();
     sensorR.initialize();
-    stepDriverL.initialize();
+    sensorL.initialize();
     stepDriverR.initialize();
-    servoL.initialize();
+    stepDriverL.initialize();
     servoR.initialize();
+    servoL.initialize();
     Serial.begin(9600);
 
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, HIGH);
-    delay(2000);
-    digitalWrite(LED_BUILTIN, LOW);
     delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
 
-    if (!sensorL.getState()) {
-        // 矫正左边
-        stepDriverL.turn(Clockwise);
-        while (!sensorL.getState()) {}
-        stepDriverL.turn(Stop);
-    }
     if (!sensorR.getState()) {
         // 矫正右边
         stepDriverR.turn(Clockwise);
         while (!sensorR.getState()) {}
         stepDriverR.turn(Stop);
+    }
+    if (!sensorL.getState()) {
+        // 矫正左边
+        stepDriverL.turn(Clockwise);
+        while (!sensorL.getState()) {}
+        stepDriverL.turn(Stop);
     }
 
     MEGA996RGrip(&servoL, Close);
